7640a1fd774579d0ea81a7175dd0bdd3ec5ec17c
Pablo Guerra <pguerrabedoya@gmail.com>
2022-05-29 14:27:36 -0500

    install:heroIcons; Add:component-register

--- delivery-food/src/App.js
+++ delivery-food/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 // import AppRoutes from './routers/AppRoutes';
 import './App.css';
-import Home from './pages/Home';
+// import Home from './pages/Home';
+import Register from './pages/Register';
 
 function App() {
   return (
     // <AppRoutes />
-    <Home/>
+    // <Home/>
+    <Register/>
   );
 }
 

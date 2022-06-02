1300ed5dacc4dbbab3bd4d8ce1d48f7a02454683
Juan Hoyos <juanhoyos1347@gmail.com>
2022-06-02 16:29:53 -0500

    se agrego las rutas

--- delivery-food-backend/src/index.ts
+++ delivery-food-backend/src/index.ts
@@ -2,8 +2,12 @@ import express from 'express';
 import  swaggerUi from 'swagger-ui-express';
 import swaggerJsDoc from 'swagger-jsdoc';
 const app = express();
+import router from './routes/establecimiento.routers';
+import routerUsuario from './routes/usuarios.router';
 const port = 8070;
 
+app.use('/establecimiento',router);
+app.use('/usuario',routerUsuario);
 
 app.listen(port,()=>{
     console.log(`Server started at http://localhost:${port}`);

c531250feca6c01fe2703f7efb12936ae254c73b
Juan Hoyos <juanhoyos1347@gmail.com>
2022-06-05 23:51:53 -0500

    router usuario

--- delivery-food-backend/src/routes/usuarios.router.ts
+++ delivery-food-backend/src/routes/usuarios.router.ts
@@ -1,13 +1,15 @@
 import {Router} from 'express';
 const routerUsuario = Router();
 import { createValidator } from 'express-joi-validation';
-import {getUsers,createUser,updateUser,deleteUser} from '../controllers/usuario.controllers'
+import {getUsers,createUser,updateUser,deleteUser,getUserById} from '../controllers/usuario.controllers'
 import usuarioSchema from '../schemas/usuario.schema.joi';
+import usuarioParamSchema from '../schemas/usuario_params.schema.joi';
 const validator = createValidator();
 
 routerUsuario.get('/',getUsers);
 routerUsuario.post('/',validator.body(usuarioSchema),createUser);
-routerUsuario.put('/:id',updateUser);
-routerUsuario.delete('/:id',deleteUser);
+routerUsuario.put('/:id',validator.params(usuarioParamSchema),validator.body(usuarioSchema),updateUser);
+routerUsuario.delete('/:id',validator.params(usuarioParamSchema),deleteUser);
+routerUsuario.get('/:id',validator.params(usuarioParamSchema),getUserById);
 
 export default routerUsuario;

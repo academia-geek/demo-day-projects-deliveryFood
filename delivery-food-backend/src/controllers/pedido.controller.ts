76917e909f9ff8e8397c990ba6280804c9c12342
Juan Hoyos <juanhoyos1347@gmail.com>
2022-06-05 23:54:27 -0500

    controlador pedido

---
+++ delivery-food-backend/src/controllers/pedido.controller.ts
@@ -0,0 +1,57 @@
+import {Request,Response} from 'express';
+import {pool} from '../db/db';
+import { QueryResult } from 'pg';
+
+//Get
+export const getPedido = async (req: Request, res: Response):Promise<Response> =>{
+    try{
+        const response:QueryResult = await pool.query('SELECT * FROM pedido ORDER BY codigoOrden ASC');
+        return res.status(200).json(response.rows);
+    }catch(e){
+        console.log(e);         
+        return res.status(500).json('Internal server error');
+    }
+};
+
+//POST
+export const createPedido = async (req: Request, res:Response):Promise<Response> =>{
+    /*TODO:Servicio de crear menu de mongo */
+    const {id_usuario,id_itempedido,impuestos,tipoEntrega,valorDomicilio,estadoDelPedido,hora,fecha,valorTotal,descuento} = req.body;
+    try{
+        const response:QueryResult = await pool.query('INSERT INTO establecimiento (estado,operacional,nombre,id_menu) VALUES ($1,$2,$3,$4)',[]);
+        return res.status(200).json({
+            message:"Establecimiento creado con éxito"
+        });
+    }catch(e){
+        console.log(e);
+        return res.status(500).json('Internal server error');
+    }
+}
+
+
+export const updateEstablecimiento = async (req: Request, res: Response) => {
+    const id_establecimiento = parseInt(req.params.id);
+    /*TODO:Servicio de actualizar menu de mongo */
+    try {
+        const {estado,operacional,nombre,id_menu} = req.body;
+        const response: QueryResult = await pool.query('UPDATE usuario SET  "estado" = $1, "operacional" = $2, "nombre" = $3, "id_menu" = $4 WHERE id_establecimiento = $5', [estado,operacional,nombre,id_menu,id_establecimiento])
+        return res.json({
+            message:"Establecimiento actualizado con éxito"
+        });
+    }catch (error) {
+        console.log(error);
+        return res.status(500).json('Internal Server error');
+    }
+};
+
+export const deleteEstablecimiento = async (req: Request, res: Response) => {
+    const id_establecimiento = parseInt(req.params.id);
+    /*TODO:Servicio de eliminar menu de mongo */
+    try {
+        const response: QueryResult = await pool.query('DELETE FROM establecimiento WHERE id_establecimiento = $1', [id_establecimiento]);
+        return res.json(response.rows);
+    }catch (error) {
+        console.log(error);
+        return res.status(500).json('Internal Server error');
+    }
+};

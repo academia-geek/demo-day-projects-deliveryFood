e8683dca6e896cce5c0edba78703acd3d4cec074
ingridlo <ingridloargote@gmail.com>
2022-06-17 23:08:43 -0500

    Actualización de esquema pedido

--- delivery-food-backend/src/schemas/pedido.schema.joi.ts
+++ delivery-food-backend/src/schemas/pedido.schema.joi.ts
@@ -1,17 +1,19 @@
-import { time } from "console";
 import Joi from "joi";
 
 const pedidoSchema = Joi.object({
     id_usuario:Joi.number().required(),
-    id_itempedido:Joi.string().max(50).required(),
+    id_establecimiento:Joi.number().required(),
+    id_menu:Joi.string().required(),
+    id_platos:Joi.array().items(Joi.string()).required(),
+    cantidad:Joi.array().items(Joi.number()).required(),
     impuestos:Joi.number().required(),
     tipoEntrega:Joi.string().valid('Domicilio','Retiro').required(),
     valorDomicilio:Joi.number().required(),
-    estadoDelPedido: Joi.string().valid('Recibido','Preparando','En camino','Entregado').required(),
-    //hora:Joi.any().valid('',time),
-    fecha:Joi.date().less('now').required(),
-    valorTodos:Joi.number().required(),
+    estadoDelPedido: Joi.string().valid('En revisión','Aceptado','Preparando','En camino','Entregado').required(),
+    hora:Joi.string().regex(/^([0-9]{2})\:([0-9]{2})\:([0-9]{2})$/).required(),
+    fecha:Joi.date().less('now').required(), 
     descuento:Joi.number().required(),
+    calificacion:Joi.number(),
 })
 
 export default pedidoSchema;
